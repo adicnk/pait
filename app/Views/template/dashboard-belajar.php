dd916c0d1fd59ddf41d16052a45ce465d92bd4d0
adicnk <adicoolnkeren@gmail.com>
2022-04-24 08:57:34 +0700

    Menambahkan Profile

--- app/Views/template/dashboard-belajar.php
+++ app/Views/template/dashboard-belajar.php
@@ -57,9 +57,7 @@
                         <span class="nav-link-text ms-1">Latihan</span>
                     </a>
                 </li>
-                <li class="nav-item mt-3">
-                    <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Account pages</h6>
-                </li>
+
                 <li class="nav-item">
                     <a class="nav-link text-white " href="profile">
                         <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
@@ -69,7 +67,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white " href="./pages/sign-in.html">
+                    <a class="nav-link text-white " href="/">
                         <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                             <i class="material-icons opacity-10">logout</i>
                         </div>
